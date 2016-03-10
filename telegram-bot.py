21e3a0e572e04b105e90fef10100ec0546e8e654
Gabriel C <gciavaglia95@gmail.com>
2016-03-09 21:45:34 -0500

    added ability to send location to user

--- telegram-bot.py
+++ telegram-bot.py
@@ -8,16 +8,29 @@ file = open('Pepe_rare.png', 'rb') # open the image file
 file_info = InputFileInfo('Pepe_rare.png', file, 'image/png') # instantiate needed file info for Telegram
 photo = InputFile('photo', file_info) # instantiate photo in Telegram's InputFile format
 
+def send_help_text(message) :
+	bot.send_message(message.chat, "This is a basic telegram bot. This bot will send a photo if \"dank\" is in the message")
+	print "Help message sent"
+
+def send_about_text(message) :
+	bot.send_message(message.chat, "This bot is written in Python using a wrapper for the Telegram API written by datamachine")
+	print "About message sent"
+
+def send_author_info(message) :
+	bot.send_message(message.chat, "Created by: Andrea \"The Dank Memester\" Ciavaglia")
+	print "Author info sent"
 
 def send_meme(message) :
 	bot.send_photo(message.chat, photo)
 	print "Photo sent"
 
-def command_response(message) :
-	if cur_message.text == "/help" :
-		bot.send_message(message.chat, "This is a basic telegram bot. This bot will send a photo if \"dank\" is in the message")
-	elif cur_message.text == "/about" :
-		bot.send_message(message.chat, "This bot is written in Python using a wrapper for the Telegram API written by datamachine")
+def command_response(message) : # respond to commands typed by the user
+	commands = {"/help" : send_help_text,
+				"/about" : send_about_text,
+				"/author" : send_author_info }
+
+	if message.text in commands :
+		commands[message.text](message)
 	else :
 		bot.send_message(message.chat, "That is not a command that this bot knows")
 	
@@ -29,22 +42,32 @@ else :
 
 dank_pattern = re.compile('(\w\sdank\s\w)|(dank\s\w)|(\w\sdank)|(Dank)')
 command_pattern = re.compile('/\w')
+location_pattern = re.compile('(location -?\d\d -?\d\d)')
 
-while True : # use long polling
-	prev = current.update_id
-	updates = bot.get_updates(1, None, None).wait() # get next update
-	current = updates[-1]
-	if prev == current.update_id : # avoid redundant checks of the update by checking previous and current update_id
-		continue
+print "Bot running..."
+try :
+	while True : # use long polling
+		prev = current.update_id
+		updates = bot.get_updates(1, None, None).wait() # get next update
+		current = updates[-1]
+		if prev == current.update_id : # avoid redundant checks of the update by checking previous and current update_id
+			continue
 
-	cur_message = current.message # hold current Message object
+		cur_message = current.message # hold current Message object
 
-	if dank_pattern.search(cur_message.text) : # check message for keywords and send photo
-		send_meme(cur_message)
+		if dank_pattern.search(cur_message.text) : # check message for keywords and send photo
+			send_meme(cur_message)
 
-	if command_pattern.match(cur_message.text) : # check message for commands
-		command_response(cur_message)
-		
+		if command_pattern.match(cur_message.text) : # check message for commands
+			command_response(cur_message)
+
+		if location_pattern.match(cur_message.text) :
+			_, longitude, latitude = cur_message.text.split() # unpack array into variables for sending
+			bot.send_location(cur_message.chat, float(latitude), float(longitude))
+
+except KeyboardInterrupt :
+	print "\nBot has stopped"
+
 # TODO:
 # 	-add more photos (or ability to grab a random photo
 #	-implement command list as dictionary (try to figure out why it is writing all contents of dictionary instead of only value mapped to key)
